97477688bdf721ca7c54ce2c4c1301b0e1cd180c
suntz72 <sunyou72@hotmail.com>
2021-03-07 15:13:06 +0100

    refactoring test

--- tests/ContaoInserttagBundleTest.php
+++ tests/ContaoInserttagBundleTest.php
@@ -5,22 +5,28 @@ declare(strict_types=1);
 /*
  * This file is part of [package name].
  *
- * (c) John Doe
+ * (c) Sun You
  *
  * @license LGPL-3.0-or-later
  */
 
 namespace Suntzu72\ContaoInserttagBundle\Tests;
 
-use Suntzu72\ContaoInserttagBundle\ContentInsertagBundle;
-use PHPUnit\Framework\TestCase;
+use Suntzu72\ContaoInserttagBundle\ContaoInserttagBundle;
+use \PHPUnit\Framework\TestCase;
 
 class ContaoInserttagBundleTest extends TestCase
 {
     public function testCanBeInstantiated(): void
     {
-        $bundle = new ContentInsertagBundle();
+        $bundle = new \Suntzu72\ContaoInserttagBundle\ContaoInserttagBundle();
 
-        $this->assertInstanceOf('Suntzu72\ContentInsertagBundle\ContentInsertagBundle', $bundle);
+        $this->assertInstanceOf('Suntzu72\ContaoInserttagBundle\ContaoInserttagBundle', $bundle);
+
+    }
+
+    public function testJusttrue(): void
+    {
+        $this->assertTrue(true);
     }
 }
